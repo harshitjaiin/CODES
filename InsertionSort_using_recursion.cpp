635cd52e9e02b91fcbd6766f666989771830861c
harshitjaiin <121127054+harshitjaiin@users.noreply.github.com>
2022-12-23 00:13:50 +0530

    Create InsertionSort_using_recursion.cpp

---
+++ InsertionSort_using_recursion.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+void insertionSort(int *arr, int beginIndex, int endIndex)
+{
+    if (beginIndex == endIndex)
+        return;
+    int flag = 0;
+    int i;
+    int toSort = arr[beginIndex + 1]; // assume 1st is sorted and usko ab sorted array
+                                         // me kaha dale yeh dekhna hai
+    for (i = beginIndex; i >= 0; i--)
+    {
+            if (toSort < arr[i])
+        {
+            arr[i + 1] = arr[i];    //this basically does all the shifting
+            flag = 1;
+        }
+        else       // dry run krke dekho jaha rukoge usse ek aage wali index
+            break; // pe dalna hoga tumhe apne toSort wala element and also i
+    }              // scope me rahe islie usko global banaya hai..
+    if (flag)
+      arr[i + 1] = toSort; //dal diye toSort wala element sorted array me sahi jagah pe
+    insertionSort(arr, beginIndex + 1, endIndex);  //recursion call
+}
+int main()
+{
+    int arr[5] = {4, 6, 2, 9, -1};
+    insertionSort(arr, 0, 4);
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
